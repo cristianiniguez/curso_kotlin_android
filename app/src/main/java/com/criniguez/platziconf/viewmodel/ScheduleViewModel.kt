0d272e2ca96d105e0f764fefcdffed9b943077f2
cristianiniguez <felizpc1@gmail.com>
2021-05-09 12:28:48 -0400

    android y kotlin: ViewModel y LiveData

---
+++ app/src/main/java/com/criniguez/platziconf/viewmodel/ScheduleViewModel.kt
@@ -0,0 +1,33 @@
+package com.criniguez.platziconf.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.criniguez.platziconf.model.Conference
+import com.criniguez.platziconf.network.FirestoreService
+import com.criniguez.platziconf.network.Callback
+
+class ScheduleViewModel {
+    val firestorService = FirestoreService()
+    var listSchedule: MutableLiveData<List<Conference>> = MutableLiveData()
+    var isLoading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        getScheduleFromFirebase()
+    }
+
+    fun getScheduleFromFirebase() {
+        firestorService.getSchedule(object: Callback<List<Conference>> {
+            override fun onSuccess(result: List<Conference>?) {
+                listSchedule.postValue(result)
+                processFinished()
+            }
+
+            override fun onFailed(exception: Exception) {
+                processFinished()
+            }
+        })
+    }
+
+    fun processFinished() {
+        isLoading.value = true
+    }
+}
---
+++ app/src/main/java/com/criniguez/platziconf/viewmodel/SpeakersViewModel.kt
@@ -0,0 +1,33 @@
+package com.criniguez.platziconf.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.criniguez.platziconf.model.Speaker
+import com.criniguez.platziconf.network.Callback
+import com.criniguez.platziconf.network.FirestoreService
+
+class SpeakersViewModel {
+    val firestorService = FirestoreService()
+    var listSpeakers: MutableLiveData<List<Speaker>> = MutableLiveData()
+    var isLoading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        getSpeakersFromFirebase()
+    }
+
+    fun getSpeakersFromFirebase() {
+        firestorService.getSpeakers(object: Callback<List<Speaker>> {
+            override fun onSuccess(result: List<Speaker>?) {
+                listSpeakers.postValue(result)
+                processFinished()
+            }
+
+            override fun onFailed(exception: Exception) {
+                processFinished()
+            }
+        })
+    }
+
+    fun processFinished() {
+        isLoading.value = true
+    }
+}
