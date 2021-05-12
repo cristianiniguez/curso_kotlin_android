7adcdd134930c43889a2da4f261b35ed05edfbe0
cristianiniguez <felizpc1@gmail.com>
2021-05-11 22:13:27 -0400

    detalles de la app: creacion de un splash screen

--- app/src/main/java/com/criniguez/platziconf/view/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/criniguez/platziconf/view/ui/activities/SplashScreenActivity.kt
@@ -1,12 +1,34 @@
 package com.criniguez.platziconf.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.criniguez.platziconf.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.animation)
+        ivLogoPlatziConf.startAnimation(animation)
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+        })
     }
 }
